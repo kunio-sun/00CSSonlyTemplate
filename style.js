e2048a46e8b20d2a7949742981f96e415c3e70e4
Tanaka Kuniaki <kuni1100001@gmail.com>
2021-06-16 22:00:33 +0900

    style.js変更

--- style.js
+++ style.js
@@ -1,24 +1,21 @@
-// お問い合わせまでのスムーズスクロール
-jQuery(document).ready(function( $ ){
-    jQuery( window ).on( 'scroll', function() {
-      if ( 200 < jQuery( this ).scrollTop() ) { // 200px以上スクロールしたら
-        jQuery( '#global-nav' ).addClass( 'm_fixed' );
-      } else {
-        jQuery( '#global-nav' ).removeClass( 'm_fixed' );
-      }
-    });
-        });
-    
-    jQuery(document).ready(function( $ ){
-      
-          $('a[href^="#"]').click(function(){
-          var speed = 500;
-          var href= $(this).attr("href");
-          var target = $(href == "#" || href == "" ? 'html' : href);
-          var position = target.offset().top;
-          $("html, body").animate({scrollTop:position}, speed, "swing");
-          return false;
-        });
-      });
-    
-    
+
+
+jQuery(document).ready(function ($) {
+
+  // aタグスクロール
+  $('a[href^="#"]').click(function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    var headerHeight = $(".header").height() + 16;
+    $("html, body").animate({ scrollTop: position - headerHeight }, 500, "swing");
+    return false;
+  });
+
+  // scroll top
+  $('h1').click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 500, "swing")
+  })
+
+});
+
